b9ad28157c32fbdb2bc3be6a4a8857e514ba4430
Snehal-Tembare <snehal.tembare@synerzip.com>
2017-03-24 17:46:56 +0530

    Completed Search Result screen

--- app/src/main/java/com/example/synerzip/recircle_android/ui/ResultActivity.java
+++ app/src/main/java/com/example/synerzip/recircle_android/ui/ResultActivity.java
@@ -1,21 +1,22 @@
 package com.example.synerzip.recircle_android.ui;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.Transformation;
-import android.view.animation.TranslateAnimation;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.synerzip.recircle_android.R;
+import com.example.synerzip.recircle_android.models.Products;
+import com.example.synerzip.recircle_android.models.SearchProduct;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +31,21 @@ public class ResultActivity extends AppCompatActivity {
 //    @BindView(R.id.img)
 //    public ImageView mImgView;
 
+    private String productId = "";
+    private String manufacturerId = "";
+    private String query = "";
+    private String mFromDate = "";
+    private String mToDate = "";
+    private Bundle mIntent;
+
+    private String mName;
+    private String mPlace;
+    private String mStartDate;
+    private String mEndDate;
+
+    private ArrayList<Products> productsArrayList;
+    private SearchAdapter mSearchAdapter;
+
     @BindView(R.id.toolbar)
     public Toolbar mToolbar;
 
@@ -38,6 +54,21 @@ public class ResultActivity extends AppCompatActivity {
 
     @BindView(R.id.layout_header)
     public LinearLayout mHeaderLayout;
+
+    @BindView(R.id.lv_searched_items)
+    public RecyclerView mLvSearcedItems;
+
+    @BindView(R.id.txt_name)
+    public TextView mTxtName;
+
+    @BindView(R.id.txt_place)
+    public TextView mTxtPlace;
+
+    @BindView(R.id.txt_start_date)
+    public TextView mTxtStartDate;
+
+    @BindView(R.id.txt_end_date)
+    public TextView mTxtEndDate;
 
 
     @Override
@@ -51,6 +82,24 @@ public class ResultActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.common_white));
 
+        mIntent = getIntent().getExtras();
+        Log.v("Intent", mIntent.getParcelable("searchProduct").toString());
+        SearchProduct sd = mIntent.getParcelable("searchProduct");
+        productsArrayList = sd.getProducts();
+
+        mName = mIntent.getString("name", "");
+        mPlace = mIntent.getString("place", "");
+        mStartDate = mIntent.getString("startDate", "");
+        mEndDate = mIntent.getString("endDate", "");
+
+        mTxtName.setText(mName);
+        mTxtPlace.setText(mPlace);
+        mTxtStartDate.setText(mStartDate);
+        mTxtEndDate.setText(mEndDate);
+
+        mSearchAdapter=new SearchAdapter(this,productsArrayList);
+        mLvSearcedItems.setLayoutManager(new LinearLayoutManager(this));
+        mLvSearcedItems.setAdapter(mSearchAdapter);
     }
 
     @OnClick(R.id.layout_header)
@@ -70,7 +119,7 @@ public class ResultActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId()==android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             finish();
         }
         return true;
---
+++ app/src/main/java/com/example/synerzip/recircle_android/ui/SearchAdapter.java
@@ -0,0 +1,102 @@
+package com.example.synerzip.recircle_android.ui;
+
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.synerzip.recircle_android.R;
+import com.example.synerzip.recircle_android.models.Products;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Snehal Tembare on 24/3/17.
+ * Copyright © 2017 Synerzip. All rights reserved
+ */
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
+    private Context mContext;
+    private ArrayList<Products> productsArrayList;
+    private boolean[] favorite;
+
+    public SearchAdapter(Context context, ArrayList<Products> productsArrayList) {
+        mContext = context;
+        this.productsArrayList = productsArrayList;
+        favorite=new boolean[productsArrayList.size()];
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(mContext).inflate(R.layout.item_searched, parent, false);
+        return new ViewHolder(row);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Products product = productsArrayList.get(position);
+        holder.productName.setText(product.getProduct_info().getProduct_title());
+        holder.pricePerDay.setText("$" + product.getUser_product_info().getPrice_per_day() + "\\day");
+        Picasso.with(mContext)
+                .load(product.getProduct_info().getProduct_image_url())
+                .into(holder.imgProduct);
+        holder.ownerName.setText(product.getUser_info().getFirst_name() + " "
+                + product.getUser_info().getLast_name());
+        holder.ratings.setRating(Float.parseFloat(product.getUser_product_info()
+                .getProduct_avg_rating()));
+
+        holder.imgFavorite.setTag(position);
+
+        holder.imgFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = (int) v.getTag();
+                ImageView i= (ImageView) v;
+
+//                Drawable favImg = ContextCompat.getDrawable(mContext, R.drawable.ic_favorite_red);
+
+                if (favorite[position]) {
+                    i.setImageResource(R.drawable.ic_favorite_white);
+                    favorite[position]=false;
+                }else {
+                    i.setImageResource(R.drawable.ic_favorite_red);
+                    favorite[position]=true;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productsArrayList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imgProduct;
+        public TextView productName;
+        public TextView pricePerDay;
+        public TextView ownerName;
+        public RatingBar ratings;
+        public ImageView imgFavorite;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imgProduct = (ImageView) itemView.findViewById(R.id.img);
+            imgFavorite = (ImageView) itemView.findViewById(R.id.img_favorite);
+            productName = (TextView) itemView.findViewById(R.id.txt_name);
+            pricePerDay = (TextView) itemView.findViewById(R.id.txt_rate);
+            ownerName = (TextView) itemView.findViewById(R.id.txt_owner_name);
+            ratings = (RatingBar) itemView.findViewById(R.id.ratingbar);
+        }
+    }
+
+}
